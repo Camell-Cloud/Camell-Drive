2853e160f3af0593bf873c0a497280ef0c5e7082
Camell-Cloud <contact@camt.cloud>
2024-08-05 17:21:49 +0900

    Chart page ui Some modifications and additions

--- app/Screens/screens/ChartScreen.tsx
+++ app/Screens/screens/ChartScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions, ActivityIndicator, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { View, Text, StyleSheet, Image, Dimensions, ActivityIndicator, TouchableOpacity, Linking, Alert } from 'react-native';
 import { CandlestickChart } from 'react-native-wagmi-charts';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { useNavigation } from '@react-navigation/native';
@@ -12,36 +12,44 @@ const ChartScreen = () => {
   const [currentTime, setCurrentTime] = useState(null);
 
   useEffect(() => {
-    fetchChartData();
+    fetch('http://43.201.64.232:5000/gopax-chart')
+      .then(response => response.json())
+      .then(data => {
+        if (Array.isArray(data)) {
+          const formattedData = data.map(item => ({
+            timestamp: new Date(item[0]).getTime(),
+            open: item[3],
+            close: item[4],
+            high: item[2],
+            low: item[1],
+          }));
+          setChartData(formattedData);
+          setCurrentPrice(formattedData[formattedData.length - 1].close);
+          setCurrentTime(new Date(formattedData[formattedData.length - 1].timestamp).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true }));
+          setLoading(false);
+        } else {
+          console.error('Error: Data is not an array', data);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching chart data:', error);
+        setLoading(false);
+      });
   }, []);
 
-  const fetchChartData = async () => {
-    try {
-      const response = await fetch('http://43.201.64.232:5000/gopax-chart');
-      const data = await response.json();
-      if (Array.isArray(data)) {
-        const formattedData = data.map(item => ({
-          timestamp: new Date(item[0]).getTime(),
-          open: item[3],
-          close: item[4],
-          high: item[2],
-          low: item[1],
-        }));
-        setChartData(formattedData);
-        const latestData = formattedData[formattedData.length - 1];
-        setCurrentPrice(latestData.close);
-        setCurrentTime(new Date(latestData.timestamp).toLocaleTimeString('en-US', {
-          hour: '2-digit', minute: '2-digit', hour12: true,
-        }));
-      } else {
-        console.error('Error: Data is not an array', data);
-      }
-    } catch (error) {
-      console.error('Error fetching chart data:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const handleOpenGopaxApp = useCallback(() => {
+    const url = 'https://www.gopax.co.kr/exchange?market=camt-krw'; // 고팍스 앱의 구글 스토어 URL
+    Linking.canOpenURL(url)
+      .then((supported) => {
+        if (supported) {
+          Linking.openURL(url);
+        } else {
+          Alert.alert("Can't open Gopax app", "Gopax app is not installed or URL scheme is incorrect.");
+        }
+      })
+      .catch((err) => console.error('An error occurred', err));
+  }, []);
+  
 
   if (loading) {
     return (
@@ -65,7 +73,10 @@ const ChartScreen = () => {
         <TouchableOpacity onPress={() => navigation.navigate('Wallet')} style={styles.backButton}>
           <Ionicons name="arrow-back" size={36} color="black" />
         </TouchableOpacity>
-        <Image source={require('../../../assets/images/coin.png')} style={styles.logo} />
+        <Image
+          source={require('../../../assets/images/coin.png')}
+          style={styles.logo}
+        />
         <View style={styles.titleContainer}>
           <Text style={styles.title}>CAMT / KRW</Text>
           <Text style={styles.subTitle}>{currentPrice ? currentPrice.toLocaleString() : 'N/A'}</Text>
@@ -74,9 +85,9 @@ const ChartScreen = () => {
       </View>
       <View style={styles.chartContainer}>
         <CandlestickChart.Provider data={chartData}>
-          <CandlestickChart height={Dimensions.get('window').height - 300} width={Dimensions.get('window').width}>
+          <CandlestickChart height={280} width={Dimensions.get('window').width - 60}>
             <CandlestickChart.Candles positiveColor="green" negativeColor="red" />
-            <CandlestickChart.Crosshair color="black">
+            <CandlestickChart.Crosshair color='black'>
               <CandlestickChart.Tooltip />
             </CandlestickChart.Crosshair>
           </CandlestickChart>
@@ -92,6 +103,12 @@ const ChartScreen = () => {
           />
         </CandlestickChart.Provider>
       </View>
+
+      <View style={styles.buyContainer}>
+        <TouchableOpacity style={styles.buyButton} onPress={handleOpenGopaxApp}>
+          <Text style={styles.buyButtonText}>Buy</Text>
+        </TouchableOpacity>
+      </View>
       <View style={styles.currentDataContainer}>
         <Text style={{ fontSize: 16 }}>The following are Gopax's price data, from one day ago until now, in 30-minute intervals.</Text>
       </View>
@@ -104,15 +121,16 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   header: {
+    justifyContent: 'center',
     flexDirection: 'row',
     alignItems: 'center',
     padding: 10,
     backgroundColor: '#f5f5f5',
-    borderBottomWidth: 0.2,
+    borderBottomWidth: 0.2
   },
   titleContainer: {
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   title: {
     fontSize: 14,
@@ -121,16 +139,20 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   chartContainer: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     padding: 10,
+    margin: 20,
+    borderWidth: 2,
+    borderColor: '#ccc',
+    borderRadius: 20,
+    backgroundColor: '#f5f5f5',
+    backgroundColor: 'snow'
   },
   currentDataContainer: {
     justifyContent: 'center',
     padding: 5,
     borderTopWidth: 0.2,
-    flex: 0.1,
     alignItems: 'center',
   },
   currentDataText: {
@@ -163,8 +185,26 @@ const styles = StyleSheet.create({
   backButton: {
     position: 'absolute',
     left: 1,
-    color: 'black',
+    color: 'black'
   },
+  buyButton: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 50,
+  },
+  buyButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  buyContainer: {
+    alignItems: 'flex-end',
+    marginRight: 11,
+    justifyContent: 'center',
+    marginBottom: 10,
+  }
 });
 
 export default ChartScreen;
